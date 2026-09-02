659900d6b5cd10ba2ca5a548aacc507b1cc140c9
Abhishek <abhya007ab@gmail.com>
2026-09-03 01:19:35 +0530

    Revert "fix: improve signup password validation"
    This reverts commit 604b8ee9c1673101a1f341faf2aacd4e275a7284.

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
-import CustomSignUp from "@/components/auth/CustomSignUp";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <CustomSignUp />;
+  return <SignUp />;
 }
